edb0b6551d1e0f56b12917a1ece01da28fd076b5
Abdulrahman Othman <abdulrahman.othamn.sy@gmail.com>
2024-10-03 04:30:27 +0300

    apply authourization on job routes using JobPolicy

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,8 +8,9 @@
     <p>
         This job pays {{ $job->salary }} per year.
     </p>
-
+    @can('edit' , $job)
     <p class="mt-6">
        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
     </p>
+    @endcan
 </x-layout>
